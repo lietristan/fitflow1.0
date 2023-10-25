9c048c021c1ce46df91a6483a9313b649952a970
NicholasOwenW <nicholas.owen@student.umn.ac.id>
2023-10-25 12:04:02 +0700

    Login Page Done (0 Func), Intent to homeActivity

--- app/src/main/java/com/example/fitflow10/MainActivity.kt
+++ app/src/main/java/com/example/fitflow10/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.fitflow10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.loginButton).setOnClickListener() {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
