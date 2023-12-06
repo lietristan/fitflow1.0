5e91e7a2eabebc1c02a8740a143cbdcbb4fb673b
lietristan <115028927+lietristan@users.noreply.github.com>
2023-12-06 09:13:40 +0700

    login regis

--- app/src/main/java/com/example/project/LoginActivity.kt
+++ app/src/main/java/com/example/project/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.example.project
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -23,12 +27,49 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         }
-        val loginBtn = findViewById<Button>(R.id.btn_login)
-        loginBtn.setOnClickListener{
-            val pindahTempat = Intent(this, MainActivity::class.java)
-            startActivity(pindahTempat)
-            finish()
+
+        auth = FirebaseAuth.getInstance()
+        binding.btnLogin.setOnClickListener {
+            var email = binding.edtEmailLogin.text.toString()
+            var password = binding.edtPasswordLogin.text.toString()
+// Ketentuan Register kalau register ga diisi email
+            if (email.isEmpty()) {
+                binding.edtEmailLogin.error = "Email harus diisi"
+                binding.edtEmailLogin.requestFocus()
+                return@setOnClickListener
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.edtEmailLogin.error = "Email tidak vaid"
+                binding.edtEmailLogin.requestFocus()
+                return@setOnClickListener
+            }
+// Ketentuan kalau ga isi password
+            if (password.isEmpty()) {
+                binding.edtPasswordLogin.error = "Password harus diisi"
+                binding.edtPasswordLogin.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.length < 8) {
+                binding.edtPasswordLogin.error = "Password harus 8 karakter"
+                binding.edtPasswordLogin.requestFocus()
+                return@setOnClickListener
+            }
+            loginUserFirebase(email, password)
         }
+    }
+
+    private fun loginUserFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "$email Berhasil Login", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "$email Gagal Login", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
 
